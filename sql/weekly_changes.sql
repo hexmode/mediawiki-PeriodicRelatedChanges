d67311d1a765a0deee58668856179589f5b21730
Mark A. Hershberger <mah@everybody.org>
2016-12-17 15:10:03 -0500

    Add indices for uniqueness

--- sql/weekly_changes.sql
+++ sql/weekly_changes.sql
@@ -4,3 +4,7 @@ CREATE TABLE /*_*/weekly_changes (
   wc_page int unsigned NOT NULL,
   wc_timestamp binary(14) default NULL
 ) /*$wgDBTableOptions*/;
+
+CREATE UNIQUE INDEX /*i*/wc_user_page ON /*_*/weekly_changes (wc_user, wc_page);
+CREATE INDEX /*i*/wc_user ON /*_*/weekly_changes (wc_user);
+CREATE INDEX /*i*/wc_page ON /*_*/weekly_changes (wc_page);
