c36ec82ea3f9cd45f3b90392202563e76331449e
Mark A. Hershberger <mah@everybody.org>
2017-06-01 11:57:55 -0400

    Flesh out
    * mail option started
    * days option added
    * improve description
    * Provide diff link.

--- maintenance/listWatch.php
+++ maintenance/listWatch.php
@@ -38,14 +38,20 @@ require_once $maint;
 
 class ListWatch extends Maintenance {
 
+	// The number of days to look back
+	protected $days;
+
 	/**
 	 * The constructor, of course.
 	 */
 	public function __construct() {
 		parent::__construct();
-		$this->addDescription( "Show a watch." );
-		$this->addArg( "user", "User to list watch for.",
-					   true );
+		$this->addDescription( "Given a user, show all that user's watches.  Provided" .
+							   "with a title as well, show the related page changes" );
+		$this->addOption( "days", "How many days back to look (7)", false, true, "d" );
+		$this->addOption( "mail", "Send an email instead of printing out.",
+						  false, false, "m" );
+		$this->addArg( "user", "User to list watch for.", true );
 		$this->addArg( "page", "The watch to show a report for.  If not " .
 					   "given, list the user's watches.",
 					   false );
@@ -71,44 +77,155 @@ class ListWatch extends Maintenance {
 
 		$watches = RelatedChangeWatchList::newFromUser( $user );
 
+		if ( $this->hasOption( "mail" ) ) {
+			$watches->sendEmail();
+			return;
+		}
+
+		if ( $page === null ) {
+			$this->printWatchedTitles( $user, $watches );
+			return;
+		}
+
+		$this->days = $this->getOption( "days", 7 );
+
+		$this->printRelatedChanges( $user, $watches, $page );
+	}
+
+	/**
+	 * Produce a report of related changes for a page
+	 * @param User $user to print for
+	 * @param RelatedChangeWatchList $watches object
+	 * @param WikiPage $page to examine
+	 * @return null
+	 */
+	protected function printRelatedChanges(
+		User $user, RelatedChangeWatchList $watches, WikiPage $page
+	) {
+		$title = $page->getTitle();
+		$changesTo = $watches->getChangesFor( $page, $this->days, "to" );
+		$changesFrom = $watches->getChangesFor( $page, $this->days, "from" );
+		if ( !$changesTo && !$changesFrom ) {
+			$this->error( "$user has no changes on $title!\n", 1 );
+		}
+
+		ksort( $changesTo );
+		ksort( $changesFrom );
+
+		$width = $this->getLongestInList(
+			5, array_keys( array_merge( $changesTo, $changesFrom ) ),
+						   function( $arg ) {
+							   return $arg;
+						   } );
+
+		$this->output( "Changes for $title:\n" );
+		$this->output( "Title" . str_repeat( " ", $width - 4 ) );
+		$this->output( "# revs  Diff URL\n" );
+		$this->output( "-----" . str_repeat( "-", $width - 4 ) );
+		$this->output( "------------------\n" );
+		$this->output( "  Linked From:\n" );
+		foreach ( $changesFrom as $title => $change ) {
+			$this->printChange( $width, $title, $change );
+		}
+		$this->output( "\n  Linked To:\n" );
+		foreach ( $changesTo as $title => $change ) {
+			$this->printChange( $width, $title, $change );
+		}
+	}
+
+	/**
+	 * Produce a report of the titles being watched changes for a page
+	 * @param User $user to print for
+	 * @param RelatedChangeWatchList $watches object
+	 * @return null
+	 */
+	protected function printWatchedTitles( User $user, RelatedChangeWatchList $watches ) {
 		if ( $watches->numRows() === 0 ) {
 			$this->error( "$user does not have any periodic notices!\n", 1 );
 		}
 
-		if ( $page === null ) {
-			$this->output( "$user has these periodic notices:\n" );
-			$this->output( "Title\t\tLast Seen\n" );
-			$this->output( "------------------------------\n" );
-			iterator_apply( $watches, [ $this, 'printWatch' ], [ $watches ] );
-			return;
-		}
+		$width = $this->getLongestInList( 5, $watches, function ( $arg ) {
+			return $arg['page']->getTitle();
+		} );
 
-		$changes = $watches->getChangesFor( $page );
-
-		if ( $changes->numRows() === 0 ) {
-			$this->error( "$user has no changes on $page!\n", 1 );
-		}
-
-		$this->output( "Changes for $page:\n" );
-		$this->output( "Title\t\tLast Seen\t\tDiff URL" );
+		$this->output( "$user has these periodic notices:\n" );
+		$this->output( "Title" . str_repeat( " ", $width - 4 ) );
+		$this->output( "Last Seen\n" );
 		$this->output( "------------------------------\n" );
-		iterator_apply( $changes, [ $this, 'printChange' ], [ $changes ] );
+		foreach ( $watches as $watch ) {
+			$this->printWatch( $width, $watch );
+		}
+	}
+
+	/**
+	 * Get the longest string
+	 * @param int $init integer to start
+	 * @param array|Iterator $list to look in
+	 * @param function $extractor callback to get the value
+	 * @return int length of longest
+	 */
+	protected function getLongestInList( $init, $list, $extractor ) {
+		$longest =  $init;
+		foreach ( $list as $item ) {
+			$length = strlen( call_user_func( $extractor, $item ) );
+			if ( $length > $longest ) {
+				$longest = $length;
+			}
+		}
+		return $longest;
 	}
 
 	/**
 	 * Handle the display of a single watch name.
 	 *
-	 * @param Iterator $obj a single watch
+	 * @param int $width to pad to
+	 * @param array $res a single watch
 	 * @return bool
 	 */
-	public function printWatch( Iterator $obj ) {
-		$res = $obj->current();
-		$title = $res['page']->getTitle();
+	public function printWatch( $width, $res ) {
+		$titleLen = strlen( $res['page']->getTitle() );
+		$title = $res['page']->getTitle()
+			   . str_repeat( " ", $width - $titleLen + 1 );
 		$timestamp = $res['timestamp'] ? $res['timestamp'] : "(not checked)";
-		$this->output( "$title\t" );
+		$this->output( $title );
 		$this->output( "$timestamp\n" );
 
 		return true;
+	}
+
+	/**
+	 * Handle the display of a list of changes
+	 *
+	 * @param int $width to pad to
+	 * @param string $title to display
+	 * @param array $res a list of changes
+	 * @return bool
+	 */
+	public function printChange( $width, $title, $res ) {
+		$titleLen = strlen( $title );
+		$title = $title
+			   . str_repeat( " ", $width - $titleLen + 1 );
+		$count = count( $res['ts'] );
+
+		$old = $res['old'];
+		$new = $res['new'];
+
+		$this->output( $title . sprintf( " %5d  ", $count ) .
+					   $this->getDiffLink( $old, $new ) . "\n" );
+
+		return true;
+	}
+
+	/**
+	 * Return the link needed to see this group of diffs
+	 * @param int $old revision #
+	 * @param int $new revision #
+	 * @return string
+	 */
+	protected function getDiffLink( $old, $new ) {
+		global $wgServer, $wgScript;
+
+		return $wgServer . $wgScript . "?diff=$new&oldid=$old";
 	}
 }
 
